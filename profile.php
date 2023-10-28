f1defd874d9e71eb4abb9d20995581a420ca51d1
Amanbek0001 <113575543+Amanbek0001@users.noreply.github.com>
2023-10-28 13:31:15 +0600

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet"  href="Home/CSS/reset.css">
+    <link rel="stylesheet"  href="Home/CSS/style.css">
+</head>
+<body>
+    <header class="header">
+        <div class="wrapper">
+                <div class="header_wrapper">
+
+                    <div class="header_logo"> 
+                        <a href="/" class="header_logo_link">
+                            <img src="Home/Images/2.png" alt="Logotyp" class="header_logo_picture">
+                        </a>
+                    </div>
+                
+
+                     <nav class="header__nav">
+                         <ul class="header__list">
+                             <li class="header__items"><a href="" class="header__links">Mázir</a></li>
+                             <li class="header__items"><a href="" class="header__links">Online Kitap</a></li>
+                             <li class="header__items"><a href="" class="header__links">Janr</a></li>
+                             <li class="header__items"><a href="" class="header__links">Jańa ádebıetter</a></li>
+                             <li class="header__items"><a href="" class="header__links">Satyp alý</a></li>
+                         </ul>
+                     </nav>
+
+                     <form>
+                         <a href="vendor/logout.php" class="logout">Shyǵý</a>
+                     </form>
+                 
+                </div>
+
+            </header>
+
+            <!--main-->
+
+            <main class="main">
+                <section class="intro">
+                    <img src="Home/1.jpg" alt="Photo" class="intro">
+                    <img src="Home/Images/cloud.png" class="cloud">
+                    <img src="Home/Images/cloud.png" class="cloud2">
+                </section>
+            </main>
+
+            <div class="post_wrapper">
+
+                <div class="post_post" data-number="01">
+                    <h3 class="post_title">DoDo</h3>
+
+                    <p class="post_subtitle">Answer a quick survey about how you express yourself, what causes you stress, and what area would you like to work on. This way, we can fully personalize your journal!</p>
+
+                    <img class="post_img" src="Home/Images/1.jpg" alt="">
+
+                </div>
+
+                <div class="post_post" data-number="02">
+                    <h3 class="post_title">DoDo</h3>
+
+                    <p class="post_subtitle">Write, draw, reflect, understand. Alivio will guide you through the prompts and will help you manage your stress!</p>
+
+                    <img class="post_img" src="Home/Images/2.jpg" alt="">
+
+                </div>
+
+                <div class="post_post" data-number="03">
+                    <h3 class="post_title">DoDo!</h3>
+
+                    <p class="post_subtitle">Now you are aware, and have a way to manage and overcome your own stress.
+                        What are you waiting for? Alivio today!</p>
+
+                    <img class="post_img" src="Home/Images/3.jpg" alt="">
+
+                </div>
+
+        </div>
+
+    </div>
+</body>
+</html>
